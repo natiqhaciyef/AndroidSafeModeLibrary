59fb0e6168e458711daab086d4d697d9c6c8869e
natiqhaciyef <natiq00h2272@gmail.com>
2025-01-14 22:32:06 +0100

    Language Model

--- android-safe-mode-util/src/main/java/com/natiqhaciyef/android_safe_mode_util/models/ui/LanguageModel.kt
+++ android-safe-mode-util/src/main/java/com/natiqhaciyef/android_safe_mode_util/models/ui/LanguageModel.kt
@@ -6,6 +6,8 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class LanguageModel(
     val name: String,
+    val detailedName: String,
     val image: String? = null,
     val level: String,
+    val isSelected: Boolean = false
 ): Parcelable
