2cbadf59a24a09562a7b912f50e0486eae84e0a3
natiqhaciyef <natiq00h2272@gmail.com>
2025-01-14 23:19:29 +0100

    Language Model update

--- android-safe-mode-util/src/main/java/com/natiqhaciyef/android_safe_mode_util/models/ui/LanguageModel.kt
+++ android-safe-mode-util/src/main/java/com/natiqhaciyef/android_safe_mode_util/models/ui/LanguageModel.kt
@@ -1,12 +1,13 @@
 package com.natiqhaciyef.android_safe_mode_util.models.ui
 
 import android.os.Parcelable
+import com.natiqhaciyef.android_safe_mode_util.constants.EMPTY_STRING
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class LanguageModel(
     val name: String,
-    val detailedName: String,
+    val detailedName: String = EMPTY_STRING,
     val image: String? = null,
     val level: String,
     val isSelected: Boolean = false
