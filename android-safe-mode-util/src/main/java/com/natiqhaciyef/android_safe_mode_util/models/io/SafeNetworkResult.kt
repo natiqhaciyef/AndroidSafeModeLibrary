d28a025a318944c386945b3ba79ed2834ddbbf34
natiqhaciyef <natiq00h2272@gmail.com>
2025-05-10 17:31:38 +0200

    SafeNetworkResult extension function created for handling response more faster.

--- android-safe-mode-util/src/main/java/com/natiqhaciyef/android_safe_mode_util/models/io/SafeNetworkResult.kt
+++ android-safe-mode-util/src/main/java/com/natiqhaciyef/android_safe_mode_util/models/io/SafeNetworkResult.kt
@@ -4,3 +4,11 @@ data class SafeNetworkResult<T: Any>(
     val result: CrudModel,
     val data: T?
 )
+
+fun <T: Any> SafeNetworkResult<T>.handleResponse(): T?{
+    if (data == null || result.resultCode !in 200..299){
+        return null
+    }
+
+    return data
+}
